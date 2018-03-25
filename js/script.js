9fe2bc8fa92e9b2a5dce52f31facd2a3b37c8f6a
Ming Zhao Zheng <mingzhao357@gmail.com>
2018-03-24 23:15:02 -0400

    setup the route api with dummy results

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
-// const result = #;
-const jsonFile = [1,0.85];
+function sendLink() {
+	let link = document.getElementById("link").value;
+	const getUrl = window.location;
+	const url = `${getUrl.protocol}//${getUrl.host}/api/v1/ml?link=${link}`;
+
+	fetch(url, {method: 'GET'})
+		.then(res => res.json())
+		.then(json => verify(json))
+}
+
 function verify(jsonFile){
-	if (jsonFile[0] === 1){
+	if (jsonFile['trustWorthy'] === 1){
 		document.getElementById("score").innerHTML="Article Fiable!";
 
 	} else {
 
 	}
-
-
-}
-function hideDiv1(){
-	document.getElementById("p1").style.display = "none";
-	document.getElementById("p2").style.display = "block";
-	verify(jsonFile);
-}
-function hideDiv2(){
-	document.getElementById("p1").style.display = "block";
-	document.getElementById("p2").style.display = "none";
 }
 
+
+function hideDiv(action){
+	const analyse = ["none", "block", sendLink()];
+	const back = ["block", "none", () => {document.getElementById("score").innerHTML=""}]
+
+	let now = [];
+	if (action === "analyse")
+		now = analyse
+	else if (action === "retourner")
+		now = back
+
+	try {
+		document.getElementById("p1").style.display = now[0];
+		document.getElementById("p2").style.display = now[1];
+		now[2];
+	} catch(e) {
+		console.log(e);
+	}
+}
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, json, request
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
@@ -16,6 +16,23 @@ def send_css(path):
 @app.route('/assets/<path:path>')
 def send_assets(path):
     return send_from_directory('assets', path)
+
+@app.route('/api/v1/ml')
+def mlApi():
+	link = request.args.get('link')
+	print('link = ' + str(link))
+	# xh function
+	xh = (1, 0.5)
+	data = json.loads(json.dumps({'trustWorthy': xh[0], "trustLevel": xh[1]}))
+	# anna function
+	anna = json.loads('{"mydata": 1, "anotherData": 2}')
+	for key in anna.keys():
+		data[key] = anna.get(key)
+
+	print(data)
+	# retVal = {key: value for (key, value) in (xhData.items() + anna.items())}
+	# print("returnValue = " + str(retVal))
+	return json.dumps(data)
 
 if __name__ == '__main__':
 	app.run()
