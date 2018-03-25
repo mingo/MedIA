c4372f639b5cc188c422ff167789a708351f3d96
Yunke Xiao <33202018+BrotherYunke@users.noreply.github.com>
2018-03-24 22:51:28 -0400

    temp

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+// const result = #;
+const jsonFile = [1,0.85];
+function verify(jsonFile){
+	if (jsonFile[0] === 1){
+		document.getElementById("score").innerHTML="Article Fiable!";
 
-function hello() {
-	alert("HELLOOOO");
+	} else {
+
+	}
+
+
+}
+function hideDiv1(){
+	document.getElementById("p1").style.display = "none";
+	document.getElementById("p2").style.display = "block";
+	verify(jsonFile);
+}
+function hideDiv2(){
+	document.getElementById("p1").style.display = "block";
+	document.getElementById("p2").style.display = "none";
 }
 
