eed19f3197e09d42b5eaeb595f5dc49d705f0809
Yunke Xiao <33202018+BrotherYunke@users.noreply.github.com>
2018-03-25 00:41:56 -0400

    JS

--- js/script.js
+++ js/script.js
@@ -1,32 +1,12 @@
-function sendLink() {
-	let link = document.getElementById("link").value;
-	const getUrl = window.location;
-	const url = `${getUrl.protocol}//${getUrl.host}/api/v1/ml?link=${link}`;
-
-	fetch(url, {method: 'GET'})
-		.then(res => res.json())
-		.then(json => verify(json))
-}
-
-function verify(jsonFile){
-	if (jsonFile['trustWorthy'] === 1){
-		document.getElementById("score").innerHTML="Article Fiable!";
-
-	} else {
-
-	}
-}
-
-
 function hideDiv(action){
 	const analyse = ["none", "block", sendLink()];
-	const back = ["block", "none", () => {document.getElementById("score").innerHTML=""}]
+	const back = ["block", "none", () => {document.getElementById("score").innerHTML=""}];
 
 	let now = [];
 	if (action === "analyse")
-		now = analyse
+		now = analyse;
 	else if (action === "retourner")
-		now = back
+		now = back;
 
 	try {
 		document.getElementById("p1").style.display = now[0];
@@ -36,3 +16,46 @@ function hideDiv(action){
 		console.log(e);
 	}
 }
+
+
+function sendLink() {
+	let link = document.getElementById("link").value;
+	const getUrl = window.location;
+	const url = `${getUrl.protocol}//${getUrl.host}/api/v1/ml?link=${link}`;
+
+	fetch(url, {method: 'GET'})
+		.then(res => res.json())
+		.then(json => {
+			console.log(json);
+			verify(json);
+		})
+}
+
+function verify(jsonFile){
+	if (jsonFile.SiteYN === 1){
+		document.getElementById("YN").innerHTML="Article Fiable!";
+		document.getElementById("score").innerHTML=(jsonFile.trustLevel * 100) + "% niveau de confiance";
+	} else {
+		document.getElementById("YN").innerHTML="Article N'est Fiable!";
+		document.getElementById("score").innerHTML=(jsonFile.trustLevel * 100) + "% niveau de confiance";
+
+		let list = "L'article est non fiable pour les raisons suivantes:\n";
+		if (jsonFile.Explanation[0] == 1){
+			list += "- Partage de conspirations\n";
+		}
+		if (jsonFile.Explanation[1] == 1) {
+			list += "- Présentation de faits alternatifs à ceux qui sont vérifiables\n";
+		}
+		if (jsonFile.Explanation[2] == 1) {
+			list +="- Encouragement de la méfiance envers la science\n";
+		}
+		if (jsonFile.Explanation[3] == 1) {
+			list += "- Ommission d'informations qui ne conviennent pas à la vision de l'auteur";
+		}
+
+		document.getElementById("failDetail").innerHTML=list;
+		console.log(list);
+
+	}
+}
+
